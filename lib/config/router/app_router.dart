1cac6e5d4254b115c0427c351436f595edf228cd
Sasha <sashanahiarabottero@gmail.com>
2024-09-13 19:48:44 -0300

    infinite scroll terminado

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -56,6 +56,12 @@ final appRouter = GoRouter(
       builder: (context, state) => const AppTutorialScreen(),
     ),
 
+    GoRoute(
+      path: '/infinite',
+      name: InfiniteScrollScreen.name,
+      builder: (context, state) => const InfiniteScrollScreen(),
+    ),
+
 
   ],
 );
